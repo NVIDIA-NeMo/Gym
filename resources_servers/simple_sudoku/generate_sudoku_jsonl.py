162b6dfa2ab53e2d61f9e2a213653880b9978705
Rahul Chand <rchand@nvidia.com>
2025-09-17 05:11:26 -0700

    fixed linting
    Signed-off-by: Rahul Chand <rchand@nvidia.com>

--- resources_servers/simple_sudoku/generate_sudoku_jsonl.py
+++ resources_servers/simple_sudoku/generate_sudoku_jsonl.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python3
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 import random
 import sys
@@ -30,12 +43,8 @@ def replicate_jsonl(template_file: str, num_lines: int, output_file: str):
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print(
-            "Usage: python replicate_jsonl.py <template_file> <num_lines> <output_file>"
-        )
-        print(
-            "Example: python replicate_jsonl.py data/simple_sudoku.jsonl 100 data/sudoku_batch.jsonl"
-        )
+        print("Usage: python replicate_jsonl.py <template_file> <num_lines> <output_file>")
+        print("Example: python replicate_jsonl.py data/simple_sudoku.jsonl 100 data/sudoku_batch.jsonl")
         sys.exit(1)
 
     template_file, num_lines, output_file = sys.argv[1], int(sys.argv[2]), sys.argv[3]
