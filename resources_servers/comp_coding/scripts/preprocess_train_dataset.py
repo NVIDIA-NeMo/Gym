f7f0b9b2127d57e951dbaffc3229c9f4ddecb464
Brian Yu <bxyu@nvidia.com>
2025-09-26 13:17:57 -0700

    add preprocess script
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

---
+++ resources_servers/comp_coding/scripts/preprocess_train_dataset.py
@@ -0,0 +1,47 @@
+"""
+Run as:
+```bash
+HF_HOME=.cache \
+HF_TOKEN={your HF token} \
+python resources_servers/comp_coding/scripts/preprocess_train_dataset.py
+```
+"""
+
+import json
+
+from datasets import load_dataset
+
+
+ds = load_dataset("Nexusflow/comp_prog_filtered_no_function", split="train", stream=True)
+
+# Largely taken from https://github.com/NVIDIA/NeMo-Skills/blob/0af0b169ba62be9097f6362c4fb29202849ae036/nemo_skills/prompt/config/eval/livecodebench/python_codegen_reasoning.yaml
+prompt_template = """You are a helpful and harmless assistant. You should think step-by-step before responding to the instruction below.
+
+Please use python programming language only.
+
+You must use ```python for just the final solution code block with the following format:
+```python
+# Your code here
+```
+
+{question}"""
+
+
+with open("resources_servers/comp_coding/data/opencodereasoning_filtered_25k_train.jsonl", "w") as f:
+    for d in ds:
+        row = {
+            "responses_create_params": {
+                "input": [
+                    {
+                        "role": "user",
+                        "content": prompt_template.format(question=d["question"]),
+                    },
+                ],
+            },
+            "verifier_metadata": {"unit_tests": json.loads(d["unit_tests"])},
+            # Carry over original columns, even though they are unused for Gym
+            "hash_id": d["hash_id"],
+            "dataset": d["dataset"],
+            "source": d["source"],
+        }
+        f.write(json.dumps(row) + "\n")
