a633df528c2167d027315732db28e24770bc86e9
Brian Yu <bxyu@nvidia.com>
2025-12-10 14:10:56 -0800

    clean routing logic
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- nemo_gym/cli.py
+++ nemo_gym/cli.py
@@ -63,22 +63,16 @@ def _setup_env_command(dir_path: Path, global_config_dict: DictConfig) -> str:  
 
     uv_venv_cmd = f"uv venv --seed --allow-existing --python {global_config_dict[PYTHON_VERSION_KEY_NAME]} .venv"
 
-    pyproject_toml = False
-    requirements_txt = False
-    try:
-        with open(f"{dir_path / 'pyproject.toml'}", "r") as _f:
-            pyproject_toml = True
-    except OSError:
-        pass
-    try:
-        with open(f"{dir_path / 'requirements.txt'}", "r") as _f:
-            requirements_txt = True
-    except OSError:
-        pass
+    has_pyproject_toml = exists(f"{dir_path / 'pyproject.toml'}")
+    has_requirements_txt = exists(f"{dir_path / 'requirements.txt'}")
 
-    if pyproject_toml:
+    if has_pyproject_toml and has_requirements_txt:
+        raise RuntimeError(
+            f"Found both pyproject.toml and requirements.txt for uv venv setup in server dir: {dir_path}. Please only use one or the other!"
+        )
+    elif has_pyproject_toml:
         install_cmd = f"""uv pip install '-e .' {" ".join(head_server_deps)}"""
-    elif requirements_txt:
+    elif has_requirements_txt:
         install_cmd = f"""uv pip install -r requirements.txt {" ".join(head_server_deps)}"""
     else:
         raise RuntimeError(f"Missing pyproject.toml or requirements.txt for uv venv setup in server dir: {dir_path}")
