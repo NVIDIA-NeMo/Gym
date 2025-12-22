d4878bc0dc4e469597ad4f409cfab1a08cbac4e1
Peter Jin <pjin@nvidia.com>
2025-12-22 11:04:35 -0800

    fix genrm comparison strategy and calendar env
    Signed-off-by: Jiaqi Zeng <jiaqiz@nvidia.com>
    Signed-off-by: Peter Jin <pjin@nvidia.com>

--- resources_servers/equivalence_llm_judge/app.py
+++ resources_servers/equivalence_llm_judge/app.py
@@ -59,7 +59,9 @@ class LLMJudgeResourcesServerConfig(BaseResourcesServerConfig):
     name: str = "equivalence_llm_judge"
     judge_model_server: ModelServerRef
     judge_responses_create_params: NeMoGymResponseCreateParamsNonStreaming
-    judge_endpoint_max_concurrency: Optional[int] = None
+
+    # Concurrency limit for judge endpoint requests. Set to None to disable limiting.
+    judge_endpoint_max_concurrency: Optional[int] = 64
 
     judge_system_message: Optional[str] = None
     judge_prompt_template_fpath: str = "prompt_templates/equivalence_llm_judge.txt"
