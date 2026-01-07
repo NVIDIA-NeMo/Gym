7debc9b4c1430073dc09a9d23265ac67a03a919c
Brian Yu <bxyu@nvidia.com>
2026-01-07 11:13:34 -0800

    remove
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- responses_api_models/local_vllm_model/app.py
+++ responses_api_models/local_vllm_model/app.py
@@ -117,10 +117,6 @@ class LocalVLLMModel(VLLMModel):
         # Pass through signal setting not allowed in threads.
         signal.signal = lambda *args, **kwargs: None
 
-        # TODO remove
-        import vllm
-
-        vllm
         self._server_thread = Thread(target=uvloop.run, args=(run_server(final_args),), daemon=True)
         self._server_thread.start()
 
