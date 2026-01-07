2d428903d9c7ba5cdafea3d7620bdc8688e13772
Brian Yu <bxyu@nvidia.com>
2026-01-07 14:28:05 -0800

    .get
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- responses_api_models/local_vllm_model/app.py
+++ responses_api_models/local_vllm_model/app.py
@@ -136,7 +136,7 @@ class LocalVLLMModel(VLLMModel):
         original_RuntimeEnv = runtime_env.RuntimeEnv
 
         def new_RuntimeEnv(*args, **kwargs):
-            print("Current py_executable", kwargs["py_executable"])
+            print("Current py_executable", kwargs.get("py_executable"))
             print("New py_executable", sys.executable)
             return original_RuntimeEnv(*args, **kwargs, py_executable=sys.executable)
 
