38a61356af97ea47839b9d4e52c5a1c8ab422071
Brian Yu <bxyu@nvidia.com>
2026-01-23 14:44:26 -0800

    print tool
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- responses_api_agents/swe_agents/utils.py
+++ responses_api_agents/swe_agents/utils.py
@@ -390,6 +390,8 @@ def convert_tools_to_function_format(raw_tools: List[Dict]) -> List:
         # Convert to Response FunctionTool format which is flat
         if tool.get("type") == "function" and "function" in tool:
             func_def = tool["function"]
+            # TODO remove
+            print(func_def, file=sys.stderr)
             tools.append(
                 dict(
                     type="function",
