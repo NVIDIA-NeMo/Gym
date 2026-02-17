afb31131d50b706f1586b9d27ed3b75576e4ca8a
Brian Yu <bxyu@nvidia.com>
2026-02-16 20:41:31 -0800

    add responses native model configs
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- nemo_gym/e2e_rollout_collection.py
+++ nemo_gym/e2e_rollout_collection.py
@@ -14,8 +14,7 @@
 # limitations under the License.
 
 from asyncio import run
-from copy import deepcopy
-from typing import Any, Dict, List, Optional
+from typing import List
 
 from omegaconf import DictConfig, OmegaConf
 from pydantic import BaseModel
@@ -38,152 +37,15 @@ class Eval(BaseModel):
     rollout_collection_config: RolloutCollectionConfig
 
 
-class ModelEvalConfig(BaseModel):
-    model_short_name_for_upload: str
-    initial_global_config_dict: Dict[str, Any]
-    spinup_command: Optional[str] = None
-
-
 EVALS: List[Eval] = []
-
-
-MODEL_EVAL_CONFIGS: List[ModelEvalConfig] = [
-    ModelEvalConfig(
-        model_short_name_for_upload="gpt-oss-20b-reasoning-high",
-        initial_global_config_dict={
-            "policy_base_url": "???",
-            "policy_api_key": "???",
-            "policy_model_name": "openai/gpt-oss-20b",
-            "config_paths": [
-                "responses_api_models/openai_model/configs/openai_model.yaml",
-            ],
-            "num_samples_in_parallel": 128,
-            "responses_create_params": {
-                "reasoning": {
-                    "effort": "high",
-                },
-            },
-        },
-        spinup_command=r"""HF_HUB_OFFLINE=1 \
-HF_HOME=.cache/ \
-HOME=. \
-vllm serve \
-    openai/gpt-oss-20b \
-    --dtype auto \
-    --tensor-parallel-size 8 \
-    --gpu-memory-utilization 0.9 \
-    --enable-auto-tool-choice --tool-call-parser openai \
-    --host 0.0.0.0 \
-    --port 10240""",
-    ),
-    ModelEvalConfig(
-        model_short_name_for_upload="gpt-oss-120b-reasoning-low",
-        initial_global_config_dict={
-            "policy_base_url": "???",
-            "policy_api_key": "???",
-            "policy_model_name": "openai/gpt-oss-120b",
-            "config_paths": [
-                "responses_api_models/openai_model/configs/openai_model.yaml",
-            ],
-            "responses_create_params": {
-                "reasoning": {
-                    "effort": "low",
-                },
-                # From https://huggingface.co/openai/gpt-oss-120b/discussions/21#6892bbe3342676ebf6ba7428
-                "temperature": 1.0,
-                "top_p": 1.0,
-                "max_output_tokens": 131072,
-            },
-        },
-        spinup_command=r"""HF_HUB_OFFLINE=1 \
-HF_HOME=.cache/ \
-HOME=. \
-vllm serve \
-    openai/gpt-oss-120b \
-    --dtype auto \
-    --tensor-parallel-size 8 \
-    --gpu-memory-utilization 0.9 \
-    --enable-auto-tool-choice --tool-call-parser openai \
-    --host 0.0.0.0 \
-    --port 10240""",
-    ),
-    ModelEvalConfig(
-        model_short_name_for_upload="gpt-oss-120b-reasoning-medium",
-        initial_global_config_dict={
-            "policy_base_url": "???",
-            "policy_api_key": "???",
-            "policy_model_name": "openai/gpt-oss-120b",
-            "config_paths": [
-                "responses_api_models/openai_model/configs/openai_model.yaml",
-            ],
-            "responses_create_params": {
-                "reasoning": {
-                    "effort": "medium",
-                },
-                # From https://huggingface.co/openai/gpt-oss-120b/discussions/21#6892bbe3342676ebf6ba7428
-                "temperature": 1.0,
-                "top_p": 1.0,
-                "max_output_tokens": 131072,
-            },
-        },
-        spinup_command=r"""HF_HUB_OFFLINE=1 \
-HF_HOME=.cache/ \
-HOME=. \
-vllm serve \
-    openai/gpt-oss-120b \
-    --dtype auto \
-    --tensor-parallel-size 8 \
-    --gpu-memory-utilization 0.9 \
-    --enable-auto-tool-choice --tool-call-parser openai \
-    --host 0.0.0.0 \
-    --port 10240""",
-    ),
-    ModelEvalConfig(
-        model_short_name_for_upload="gpt-oss-120b-reasoning-high",
-        initial_global_config_dict={
-            "policy_base_url": "???",
-            "policy_api_key": "???",
-            "policy_model_name": "openai/gpt-oss-120b",
-            "config_paths": [
-                "responses_api_models/openai_model/configs/openai_model.yaml",
-            ],
-            "responses_create_params": {
-                "reasoning": {
-                    "effort": "high",
-                },
-                # From https://huggingface.co/openai/gpt-oss-120b/discussions/21#6892bbe3342676ebf6ba7428
-                "temperature": 1.0,
-                "top_p": 1.0,
-                "max_output_tokens": 131072,
-            },
-        },
-        spinup_command=r"""HF_HUB_OFFLINE=1 \
-HF_HOME=.cache/ \
-HOME=. \
-vllm serve \
-    openai/gpt-oss-120b \
-    --dtype auto \
-    --tensor-parallel-size 8 \
-    --gpu-memory-utilization 0.9 \
-    --enable-auto-tool-choice --tool-call-parser openai \
-    --host 0.0.0.0 \
-    --port 10240""",
-    ),
-]
 
 
 async def main():
     global_config_dict = get_global_config_dict()
     eval_config = RunEvalConfig.model_validate(global_config_dict)
 
-    model_eval_config = [
-        c for c in MODEL_EVAL_CONFIGS if c.model_short_name_for_upload == eval_config.model_short_name_for_upload
-    ]
-    assert len(model_eval_config) == 1, len(model_eval_config)
-    model_eval_config = model_eval_config[0]
-
     eval_config_paths = [e.config_path for e in EVALS]
-    initial_global_config_dict = deepcopy(model_eval_config.initial_global_config_dict)
+    initial_global_config_dict = dict()
     initial_global_config_dict["config_paths"] += eval_config_paths
     global_config_dict_parser_config = GlobalConfigDictParserConfig(
         initial_global_config_dict=DictConfig(initial_global_config_dict),
