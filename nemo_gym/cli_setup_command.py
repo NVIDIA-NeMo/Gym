52aaaffd22c6270e3c30478bc4b803004bdcb078
Brian Yu <bxyu@nvidia.com>
2026-02-20 17:29:20 -0800

    start refactor
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- nemo_gym/cli_setup_command.py
+++ nemo_gym/cli_setup_command.py
@@ -12,6 +12,7 @@ from nemo_gym.global_config import (
     SKIP_VENV_IF_PRESENT_KEY_NAME,
     UV_CACHE_DIR_KEY_NAME,
     UV_PIP_SET_PYTHON_KEY_NAME,
+    UV_VENV_DIR_KEY_NAME,
     get_global_config_dict,
 )
 
@@ -19,10 +20,13 @@ from nemo_gym.global_config import (
 def setup_env_command(dir_path: Path, global_config_dict: DictConfig, prefix: str) -> str:
     head_server_deps = global_config_dict[HEAD_SERVER_DEPS_KEY_NAME]
 
-    uv_venv_cmd = f"uv venv --seed --allow-existing --python {global_config_dict[PYTHON_VERSION_KEY_NAME]} .venv"
+    root_venv_path = global_config_dict[UV_VENV_DIR_KEY_NAME]
+    venv_path = Path(root_venv_path, *dir_path.parts[-2:], ".venv")
 
-    venv_python_fpath = dir_path / ".venv/bin/python"
-    venv_activate_fpath = dir_path / ".venv/bin/activate"
+    uv_venv_cmd = f"uv venv --seed --allow-existing --python {global_config_dict[PYTHON_VERSION_KEY_NAME]} {venv_path}"
+
+    venv_python_fpath = venv_path / "bin/python"
+    venv_activate_fpath = venv_path / "bin/activate"
     skip_venv_if_present = global_config_dict[SKIP_VENV_IF_PRESENT_KEY_NAME]
     should_skip_venv_setup = bool(skip_venv_if_present) and venv_python_fpath.exists() and venv_activate_fpath.exists()
 
@@ -30,17 +34,12 @@ def setup_env_command(dir_path: Path, global_config_dict: DictConfig, prefix: st
     # not needed for most clusters. should be safe in all scenarios, but only minimally tested outside of colab.
     # see discussion and examples here: https://github.com/NVIDIA-NeMo/Gym/pull/526#issuecomment-3676230383
     uv_pip_set_python = global_config_dict.get(UV_PIP_SET_PYTHON_KEY_NAME, False)
-    uv_pip_python_flag = "--python .venv/bin/python " if uv_pip_set_python else ""
+    uv_pip_python_flag = f"--python {venv_python_fpath} " if uv_pip_set_python else ""
 
     verbose_flag = "-v " if global_config_dict.get(PIP_INSTALL_VERBOSE_KEY_NAME) else ""
 
     if should_skip_venv_setup:
-        env_setup_cmd = venv_install_or_skip(
-            should_skip=True,
-            uv_venv_cmd=uv_venv_cmd,
-            install_cmd="",
-            prefix=prefix,
-        )
+        env_setup_cmd = f"source {venv_activate_fpath}"
     else:
         has_pyproject_toml = (dir_path / "pyproject.toml").exists()
         has_requirements_txt = (dir_path / "requirements.txt").exists()
@@ -56,24 +55,11 @@ def setup_env_command(dir_path: Path, global_config_dict: DictConfig, prefix: st
             raise RuntimeError(
                 f"Missing pyproject.toml or requirements.txt for uv venv setup in server dir: {dir_path}"
             )
-        env_setup_cmd = venv_install_or_skip(
-            should_skip=False,
-            uv_venv_cmd=uv_venv_cmd,
-            install_cmd=install_cmd,
-            prefix=prefix,
-        )
+
+        prefix_cmd = f" > >(sed 's/^/({prefix}) /') 2> >(sed 's/^/({prefix}) /' >&2)"
+        env_setup_cmd = f"{uv_venv_cmd}{prefix_cmd} && source {venv_activate_fpath} && {install_cmd}{prefix_cmd}"
 
     return f"cd {dir_path} && {env_setup_cmd}"
-
-
-def venv_install_or_skip(should_skip: bool, uv_venv_cmd: str, install_cmd: str, prefix: str) -> str:
-    if should_skip:
-        return "source .venv/bin/activate"
-
-    uv_venv_cmd = f"{uv_venv_cmd} > >(sed 's/^/({prefix}) /') 2> >(sed 's/^/({prefix}) /' >&2)"
-    install_cmd = f"{install_cmd} > >(sed 's/^/({prefix}) /') 2> >(sed 's/^/({prefix}) /' >&2)"
-
-    return f"{uv_venv_cmd} && source .venv/bin/activate && {install_cmd}"
 
 
 def run_command(command: str, working_dir_path: Path) -> Popen:
--- tests/unit_tests/test_cli_setup_command.py
+++ tests/unit_tests/test_cli_setup_command.py
@@ -10,7 +10,7 @@ from tests.unit_tests.test_global_config import TestServerUtils
 
 class TestCLISetupCommandSetupEnvCommand:
     def _setup_server_dir(self, tmp_path: Path) -> Path:
-        server_dir = tmp_path / "server"
+        server_dir = tmp_path / "first_level" / "second_level"
         server_dir.mkdir(parents=True)
         (server_dir / "requirements.txt").write_text("pytest\n")
         (server_dir / ".venv/bin").mkdir(parents=True)
@@ -125,7 +125,7 @@ class TestCLISetupCommandSetupEnvCommand:
         expected_command = f"cd {server_dir} && uv venv --seed --allow-existing --python test python version .venv > >(sed 's/^/(my server name) /') 2> >(sed 's/^/(my server name) /' >&2) && source .venv/bin/activate && uv pip install '-e .' ray[default]==test ray version openai==test openai version > >(sed 's/^/(my server name) /') 2> >(sed 's/^/(my server name) /' >&2)"
         assert expected_command == actual_command
 
-    def test_uv_venv_dir(self, tmp_path: Path) -> None:
+    def test_uv_venv_dir_with_install(self, tmp_path: Path) -> None:
         server_dir = self._setup_server_dir(tmp_path)
 
         uv_venv_dir = tmp_path / "uv_venv_dir"
@@ -135,7 +135,7 @@ class TestCLISetupCommandSetupEnvCommand:
             global_config_dict=self._debug_global_config_dict | {"uv_venv_dir": str(uv_venv_dir)},
             prefix="my server name",
         )
-        expected_command = f"cd {server_dir} && uv venv --seed --allow-existing --python test python version .venv > >(sed 's/^/(my server name) /') 2> >(sed 's/^/(my server name) /' >&2) && source .venv/bin/activate && uv pip install -r requirements.txt ray[default]==test ray version openai==test openai version > >(sed 's/^/(my server name) /') 2> >(sed 's/^/(my server name) /' >&2)"
+        expected_command = f"cd {server_dir} && uv venv --seed --allow-existing --python test python version {uv_venv_dir}/first_level/second_level/.venv > >(sed 's/^/(my server name) /') 2> >(sed 's/^/(my server name) /' >&2) && source {uv_venv_dir}/first_level/second_level/.venv/bin/activate && uv pip install -r requirements.txt ray[default]==test ray version openai==test openai version > >(sed 's/^/(my server name) /') 2> >(sed 's/^/(my server name) /' >&2)"
         assert expected_command == actual_command
 
 
