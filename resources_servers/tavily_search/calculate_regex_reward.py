16daf4f6bfc97bfa7b23e806e30b1162b3118cf8
Brian Yu <bxyu@nvidia.com>
2026-03-24 19:51:14 -0700

    add script to calculate regex reward
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

---
+++ resources_servers/tavily_search/calculate_regex_reward.py
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+from argparse import ArgumentParser
+
+from resources_servers.tavily_search.app import TavilySearchResourcesServer, TavilySearchVerifyRequest
+
+
+parser = ArgumentParser()
+parser.add_argument("--input_fpath", type=str, required=True)
+args = parser.parse_args()
+
+
+with open(args.input_fpath) as f:
+    dicts = list(map(json.loads, f))
+
+
+class DummySelf:
+    class config:
+        debug = False
+
+
+regex_rewards = []
+judge_rewards = []
+alignments = []
+for d in dicts:
+    verify_request = TavilySearchVerifyRequest.model_validate(d)
+    response = verify_request.response.output_text
+    regex_reward = TavilySearchResourcesServer._verify_answer_with_regex(
+        DummySelf, ground_truth=verify_request.ground_truth, response=response
+    )
+
+    judge_reward = d["reward"]
+
+    regex_rewards.append(regex_reward)
+    judge_rewards.append(judge_reward)
+    alignments.append(regex_reward == judge_reward)
+
+avg_regex_reward = sum(regex_rewards) / len(dicts)
+avg_judge_reward = sum(judge_rewards) / len(dicts)
+avg_alignment = sum(alignments) / len(dicts)
+print(f"{avg_regex_reward=:.2f} {avg_judge_reward=:.2f} {avg_alignment=:.2f}")
