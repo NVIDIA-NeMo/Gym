1daf8e4dec04d6e37b2317b3af658c0e4d2f9fb0
Brian Yu <bxyu@nvidia.com>
2026-03-26 15:52:01 -0700

    implement verification
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- resources_servers/ruler/app.py
+++ resources_servers/ruler/app.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from fastapi import FastAPI
+from typing import List, Optional
 
 from nemo_gym.base_resources_server import (
     BaseResourcesServerConfig,
@@ -27,19 +27,68 @@ class RulerResourcesServerConfig(BaseResourcesServerConfig):
     pass
 
 
+class RulerVerifyRequest(BaseVerifyRequest):
+    outputs: List[str]
+    length: int
+    subset: str
+
+
+class RulerVerifyResponse(RulerVerifyRequest, BaseVerifyResponse):
+    string_match_all_single: float
+    string_match_part_single: float
+    cwe_string_match_all_single: Optional[float]
+    cwe_string_match_part_single: Optional[float]
+    fwe_string_match_all_single: Optional[float]
+    fwe_string_match_part_single: Optional[float]
+    niah_multikey_1_string_match_all_single: Optional[float]
+    niah_multikey_1_string_match_part_single: Optional[float]
+    niah_multikey_2_string_match_all_single: Optional[float]
+    niah_multikey_2_string_match_part_single: Optional[float]
+    niah_multikey_3_string_match_all_single: Optional[float]
+    niah_multikey_3_string_match_part_single: Optional[float]
+    niah_multiquery_string_match_all_single: Optional[float]
+    niah_multiquery_string_match_part_single: Optional[float]
+    niah_multivalue_string_match_all_single: Optional[float]
+    niah_multivalue_string_match_part_single: Optional[float]
+    niah_single_1_string_match_all_single: Optional[float]
+    niah_single_1_string_match_part_single: Optional[float]
+    niah_single_2_string_match_all_single: Optional[float]
+    niah_single_2_string_match_part_single: Optional[float]
+    niah_single_3_string_match_all_single: Optional[float]
+    niah_single_3_string_match_part_single: Optional[float]
+    qa_1_string_match_all_single: Optional[float]
+    qa_1_string_match_part_single: Optional[float]
+    qa_2_string_match_all_single: Optional[float]
+    qa_2_string_match_part_single: Optional[float]
+    vt_string_match_all_single: Optional[float]
+    vt_string_match_part_single: Optional[float]
+
+
 class RulerResourcesServer(SimpleResourcesServer):
     config: RulerResourcesServerConfig
 
-    def setup_webserver(self) -> FastAPI:
-        app = super().setup_webserver()
+    async def verify(self, body: RulerVerifyRequest) -> BaseVerifyResponse:
+        prediction = body.response.output_text.strip()
+        reward_string_match_all_single = self.string_match_all_single(prediction, body.outputs)
+        reward_string_match_part_single = self.string_match_part_single(prediction, body.outputs)
 
-        # Additional server routes go here! e.g.:
-        # app.post("/get_weather")(self.get_weather)
+        return RulerVerifyResponse(
+            **body.model_dump(),
+            reward=1.0,
+            string_match_all_single=reward_string_match_all_single,
+            string_match_part_single=reward_string_match_part_single,
+            **{
+                f"{body.subset}_string_match_all_single": reward_string_match_all_single,
+                f"{body.subset}_string_match_part_single": reward_string_match_part_single,
+            },
+        )
 
-        return app
+    # These helper functions are taken from https://github.com/NVIDIA-NeMo/Skills/blob/54d2e113c2f64bf74bda72e15f23f01b524850da/nemo_skills/evaluation/evaluator/ruler.py#L36
+    def string_match_all_single(self, pred: str, refs: List[str]) -> float:
+        return sum([1.0 if r.lower() in pred.lower() else 0.0 for r in refs]) / len(refs)
 
-    async def verify(self, body: BaseVerifyRequest) -> BaseVerifyResponse:
-        return BaseVerifyResponse(**body.model_dump(), reward=1.0)
+    def string_match_part_single(self, pred: str, refs: List[str]) -> float:
+        return max([1.0 if r.lower() in pred.lower() else 0.0 for r in refs])
 
 
 if __name__ == "__main__":
