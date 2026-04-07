4a72256c29e4803857dc96c9a726cf5e7e351c82
Teodor-Dumitru Ene <teodord.ene@gmail.com>
2026-04-07 03:42:39 -0500

    Add Megatron Inference model backend
    Signed-off-by: Teodor-Dumitru Ene <teodord.ene@gmail.com>

---
+++ responses_api_models/megatron_inference/app.py
@@ -0,0 +1,64 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import List, Tuple
+
+from nemo_gym.openai_utils import RESPONSES_TO_TRAIN, NeMoGymChoice, NeMoGymResponseOutputItem, TokenIDLogProbMixin
+from nemo_gym.server_utils import is_nemo_gym_fastapi_worker
+from responses_api_models.vllm_model.app import VLLMConverter, VLLMModel
+
+
+class MegatronTokenIDLogProbMixin(TokenIDLogProbMixin):
+    policy_epoch: List[List[Tuple[int, int]]]
+    kv_cache_epoch: List[List[Tuple[int, int]]]
+    num_evictions: List[int]
+
+
+MEGATRON_RESPONSES_TO_TRAIN = {
+    base: type(f"Megatron{train.__name__}", (train, MegatronTokenIDLogProbMixin), {})
+    for base, train in RESPONSES_TO_TRAIN.items()
+}
+
+
+class MegatronInferenceConverter(VLLMConverter):
+    def postprocess_chat_response(self, choice: NeMoGymChoice) -> List[NeMoGymResponseOutputItem]:
+        response_output = super().postprocess_chat_response(choice)
+        raw_message = choice.message.model_dump()
+        if not (self.return_token_id_information and "prompt_token_ids" in raw_message):
+            return response_output
+        last = response_output[-1]
+        for base_cls, train_cls in MEGATRON_RESPONSES_TO_TRAIN.items():
+            if isinstance(last, base_cls):
+                response_output[-1] = train_cls(
+                    **last.model_dump(),
+                    policy_epoch=raw_message.get("policy_epoch", [[(0, 0)]]),
+                    kv_cache_epoch=raw_message.get("kv_cache_epoch", [[(0, 0)]]),
+                    num_evictions=raw_message.get("num_evictions", [0]),
+                )
+                break
+        return response_output
+
+
+class MegatronInferenceModel(VLLMModel):
+    def model_post_init(self, context):
+        super().model_post_init(context)
+        self._converter = MegatronInferenceConverter(
+            return_token_id_information=self.config.return_token_id_information,
+        )
+
+
+if __name__ == "__main__":
+    MegatronInferenceModel.run_webserver()
+elif is_nemo_gym_fastapi_worker():
+    app = MegatronInferenceModel.run_webserver()  # noqa: F401
