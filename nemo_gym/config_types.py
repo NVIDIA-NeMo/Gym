c05086b1941064cc6c5e839eb18a56853062f259
Tomasz Grzegorzek <tgrzegorzek@nvidia.com>
2026-06-24 10:58:36 +0200

    feat(benchmarks): Add prepare sceipt argument to types; refactor ruler prepration script
    Signed-off-by: Tomasz Grzegorzek <tgrzegorzek@nvidia.com>

--- nemo_gym/config_types.py
+++ nemo_gym/config_types.py
@@ -392,6 +392,7 @@ class BenchmarkDatasetConfig(BaseModel):
     type: Literal["benchmark"]
     jsonl_fpath: Path
     prepare_script: Path
+    prepare_script_args: Dict[str, Any] = Field(default_factory=dict)
     prompt_config: Optional[Path]
     num_repeats: int = Field(default=1, ge=1)
 
