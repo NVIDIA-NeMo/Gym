abbd4b9fbfa4e703cb172ff1061468dd662c3fdb
Brian Yu <bxyu@nvidia.com>
2026-07-09 12:35:49 -0700

    idk
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- scripts/sbatch_basic.sh
+++ scripts/sbatch_basic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+set -euo pipefail
 
 # Get the Ray head node IP
 nodes=$(scontrol show hostnames "$SLURM_JOB_NODELIST")
@@ -12,14 +12,17 @@ echo "Ray head node IP address: $RAY_HEAD_NODE_IP"
 # Symmetric run will automatically start Ray on all nodes and run the script ONLY the head node.
 # Use the '--' separator to separate Ray arguments and the entrypoint command.
 # The --min-nodes argument ensures all nodes join before running the script.
-
-# All nodes (including head and workers) will execute this block.
-# The command after '--' will only run on the head node
-srun --nodes="$SLURM_JOB_NUM_NODES" --ntasks="$SLURM_JOB_NUM_NODES" \
-    ray symmetric-run \
+ENTRYPOINT_WITH_RAY_WRAPPER=$(cat <<EOF
+ray symmetric-run \
     --address $RAY_HEAD_NODE_IP \
     --min-nodes $SLURM_JOB_NUM_NODES \
     --num-cpus=$SLURM_CPUS_PER_TASK \
     --num-gpus=$SLURM_GPUS_PER_TASK \
     -- \
-    echo "hello"
+    bash $@
+EOF
+)
+
+# All nodes (including head and workers) will execute this block.
+# The command after '--' will only run on the head node
+srun --nodes="$SLURM_JOB_NUM_NODES" --ntasks="$SLURM_JOB_NUM_NODES" bash $ENTRYPOINT_WITH_RAY_WRAPPER
