020bf051c3650d3f08493bd9f66da5d1d05d3b0e
Brian Yu <bxyu@nvidia.com>
2026-07-09 13:58:21 -0700

    idk
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- scripts/sbatch_basic.sh
+++ scripts/sbatch_basic.sh
@@ -18,22 +18,15 @@ echo "Running from $SLURM_SUBMIT_DIR"
 # Symmetric run will automatically start Ray on all nodes and run the script ONLY the head node.
 # The '--' separator is used to separate Ray arguments and the entrypoint command.
 # The --min-nodes argument ensures all nodes join before running the script.
-ENTRYPOINT_WITH_RAY_WRAPPER=$(cat <<EOF
-pwd
-ls .venv
-source .venv/bin/activate
-uv sync
 
-ray symmetric-run \
+# All nodes (including head and workers) will execute this block.
+# The command after '--' will only run on the head node
+srun --nodes=$SLURM_JOB_NUM_NODES --ntasks=$SLURM_JOB_NUM_NODES \
+    source .venv/bin/activate && uv sync \
+    && ray symmetric-run \
     --address $RAY_HEAD_NODE_IP \
     --min-nodes $SLURM_JOB_NUM_NODES \
     --num-cpus=${SLURM_CPUS_PER_TASK:-$SLURM_CPUS_ON_NODE} \
     --num-gpus=${SLURM_GPUS_PER_TASK:-$SLURM_GPUS_ON_NODE} \
     -- \
     bash $@
-EOF
-)
-
-# All nodes (including head and workers) will execute this block.
-# The command after '--' above will only run on the head node
-srun --nodes="$SLURM_JOB_NUM_NODES" --ntasks="$SLURM_JOB_NUM_NODES" bash $ENTRYPOINT_WITH_RAY_WRAPPER
