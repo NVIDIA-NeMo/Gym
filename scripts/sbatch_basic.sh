17fb395106d9097b851126fa140092ef0395c7f4
Brian Yu <bxyu@nvidia.com>
2026-07-09 13:25:53 -0700

    try alternative
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- scripts/sbatch_basic.sh
+++ scripts/sbatch_basic.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#SBATCH --output=${SLURM_JOB_ID}-${JOB_NAME}.log
+### Give all resources to a single Ray task, ray can manage the resources internally
+#SBATCH --ntasks-per-node=1
 
 set -euo pipefail
 
@@ -10,19 +13,24 @@ echo "Ray head node IP address: $RAY_HEAD_NODE_IP"
 
 # Start Ray cluster using symmetric_run.py on all nodes.
 # Symmetric run will automatically start Ray on all nodes and run the script ONLY the head node.
-# Use the '--' separator to separate Ray arguments and the entrypoint command.
+# The '--' separator is used to separate Ray arguments and the entrypoint command.
 # The --min-nodes argument ensures all nodes join before running the script.
 ENTRYPOINT_WITH_RAY_WRAPPER=$(cat <<EOF
+cd $SLURM_SUBMIT_DIR
+
+source .venv/bin/activate
+uv sync
+
 ray symmetric-run \
     --address $RAY_HEAD_NODE_IP \
-    --min-nodes \$SLURM_JOB_NUM_NODES \
-    --num-cpus=\$SLURM_CPUS_PER_TASK \
-    --num-gpus=\$SLURM_GPUS_PER_TASK \
+    --min-nodes $SLURM_JOB_NUM_NODES \
+    --num-cpus=${SLURM_CPUS_PER_TASK:-$SLURM_CPUS_ON_NODE} \
+    --num-gpus=${SLURM_GPUS_PER_TASK:-$SLURM_GPUS_ON_NODE} \
     -- \
     bash $@
 EOF
 )
 
 # All nodes (including head and workers) will execute this block.
-# The command after '--' will only run on the head node
+# The command after '--' above will only run on the head node
 srun --nodes="$SLURM_JOB_NUM_NODES" --ntasks="$SLURM_JOB_NUM_NODES" bash $ENTRYPOINT_WITH_RAY_WRAPPER
