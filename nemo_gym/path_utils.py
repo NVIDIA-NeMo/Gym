b874f0b6b2cec26bbac590af65c25548f5979af2
Martyna Patelka <mpatelka@nvidia.com>
2026-07-19 14:05:49 +0200

    fix lint

--- nemo_gym/path_utils.py
+++ nemo_gym/path_utils.py
@@ -11,7 +11,9 @@ def resolve_input_path(input_path: str | Path, error_msg: Optional[str] = None) 
         _cwd_path = Path.cwd() / input_path
         _input_path = _cwd_path if _cwd_path.exists() else PARENT_DIR / input_path
     if not _input_path.is_file():
-        error_msg = error_msg or f"Given input file not found: '{input_path}'. Check it is spelled correctly and exists."
+        error_msg = (
+            error_msg or f"Given input file not found: '{input_path}'. Check it is spelled correctly and exists."
+        )
         raise ConfigPathNotFoundError(error_msg)
     return _input_path
 
