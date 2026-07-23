db440724a0985a4107dfa5cf4569095f53a3889a
Brian Yu <bxyu@nvidia.com>
2026-07-23 11:03:16 -0700

    use fixes host and port
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- scripts/sbatch_external_vllm.sh
+++ scripts/sbatch_external_vllm.sh
@@ -24,7 +24,9 @@ vllm serve $MODEL \
     --mamba-ssm-cache-dtype float32 \
     --model-loader-extra-config '{"enable_multithread_load": true, "num_threads": 96}' \
     --moe-backend triton \
-    --max-num-batched-tokens 32768
+    --max-num-batched-tokens 32768 \
+    --host \$(hostname) \
+    --port 8000
 EOF
 )
 
