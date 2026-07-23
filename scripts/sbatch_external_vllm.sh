a3bdc0b514da85e778d8a42d506e8bfe9a54cca1
Brian Yu <bxyu@nvidia.com>
2026-07-23 13:09:15 -0700

    try newer vllm config
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- scripts/sbatch_external_vllm.sh
+++ scripts/sbatch_external_vllm.sh
@@ -3,7 +3,6 @@
 command=$(cat <<EOF
 pip install ray==2.55.1
 
-VLLM_USE_RAY_V2_EXECUTOR_BACKEND=0 \
 vllm serve $MODEL \
     --served-model-name nvidia/NVIDIA-Nemotron-3-Ultra-550B-A55B-BF16 \
     --gpu-memory-utilization 0.9 \
@@ -20,7 +19,10 @@ vllm serve $MODEL \
     --enable-chunked-prefill \
     --api-server-count 1 \
     --kv-cache-dtype fp8 \
+    --enable-flashinfer-autotune \
+    --async-scheduling \
     -cc.pass_config.fuse_allreduce_rms=False \
+    --mamba-backend triton \
     --mamba-ssm-cache-dtype float32 \
     --model-loader-extra-config '{"enable_multithread_load": true, "num_threads": 96}' \
     --moe-backend triton \
