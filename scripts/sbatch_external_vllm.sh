55441bcd7ce685601a9a8efaaaa289995a82b589
Brian Yu <bxyu@nvidia.com>
2026-07-23 15:03:33 -0700

    try 0.22 again
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- scripts/sbatch_external_vllm.sh
+++ scripts/sbatch_external_vllm.sh
@@ -3,16 +3,11 @@
 command=$(cat <<EOF
 pip install ray==2.55.1
 
+VLLM_USE_RAY_V2_EXECUTOR_BACKEND=0 \
 vllm serve $MODEL \
     --served-model-name nvidia/NVIDIA-Nemotron-3-Ultra-550B-A55B-BF16 \
-    --kv-cache-dtype fp8 \
-    --gpu-memory-utilization 0.90 \
-    --max-num-batched-tokens 32768 \
-    --enable-flashinfer-autotune \
-    --async-scheduling \
-    --mamba-backend triton \
-    --mamba-ssm-cache-dtype float32 \
-    -cc.pass_config.fuse_allreduce_rms=False \
+    --gpu-memory-utilization 0.9 \
+    --distributed-executor-backend ray \
     --data-parallel-backend ray \
     --data-parallel-size 4 \
     --data-parallel-size-local 1 \
@@ -20,35 +15,21 @@ vllm serve $MODEL \
     --enable-auto-tool-choice \
     --tool-call-parser qwen3_coder \
     --reasoning-parser nemotron_v3 \
-    --speculative-config '{"method":"mtp","num_speculative_tokens":5}'
+    --enable-prefix-caching \
+    --enable-chunked-prefill \
+    --api-server-count 1 \
+    --kv-cache-dtype fp8 \
+    -cc.pass_config.fuse_allreduce_rms=False \
+    --mamba-ssm-cache-dtype float32 \
+    --model-loader-extra-config '{"enable_multithread_load": true, "num_threads": 96}' \
+    --moe-backend triton \
+    --max-num-batched-tokens 32768 \
     --host \$(hostname -I | awk '{print \$1}') \
     --port 8000
 EOF
 )
 
-    # --gpu-memory-utilization 0.9 \
-    # --data-parallel-backend ray \
-    # --data-parallel-size 4 \
-    # --data-parallel-size-local 1 \
-    # --tensor-parallel-size 4 \
-    # --trust-remote-code \
-    # --enable-auto-tool-choice \
-    # --tool-call-parser qwen3_coder \
-    # --reasoning-parser nemotron_v3 \
-    # --enable-prefix-caching \
-    # --enable-chunked-prefill \
-    # --api-server-count 1 \
-    # --kv-cache-dtype fp8 \
-    # --enable-flashinfer-autotune \
-    # --async-scheduling \
-    # -cc.pass_config.fuse_allreduce_rms=False \
-    # --enable-expert-parallel
-    # --mamba-backend flashinfer \
-    # --mamba-ssm-cache-dtype float32 \
-    # --model-loader-extra-config '{"enable_multithread_load": true, "num_threads": 96}' \
-    # --max-num-batched-tokens 32768 \
-
-CONTAINER=/lustre/fs1/portfolios/nemotron/projects/nemotron_evals_dev/users/bxyu/vllm/vllm-openai:v0.25.1___with_ray.sqsh \
+CONTAINER=/lustre/fs1/portfolios/nemotron/projects/nemotron_evals_dev/users/bxyu/vllm/vllm-openai:v0.22.1___with_ray.sqsh \
 MOUNTS=/lustre:/lustre \
 sbatch \
     --nodes=4 \
