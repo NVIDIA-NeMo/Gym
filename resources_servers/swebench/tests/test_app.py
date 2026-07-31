0f7ee936a784724803bc145e2ee45e5127b37af3
Brian Yu <bxyu@nvidia.com>
2026-07-31 16:13:06 -0700

    fix tests
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- resources_servers/swebench/tests/test_app.py
+++ resources_servers/swebench/tests/test_app.py
@@ -12,30 +12,43 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from unittest.mock import MagicMock
+from unittest.mock import AsyncMock, MagicMock
 
 from fastapi.testclient import TestClient
+from pytest import MonkeyPatch
 
 from nemo_gym.server_utils import ServerClient
 from resources_servers.swebench.app import SwebenchResourcesServer, SwebenchResourcesServerConfig
 
 
 class TestApp:
-    def test_sanity(self) -> None:
+    def test_sanity(self, monkeypatch: MonkeyPatch) -> None:
         config = SwebenchResourcesServerConfig(
             host="0.0.0.0",
             port=8080,
             entrypoint="",
             name="",
+            sandbox_provider="test",
+            sandbox_config=dict(),
+            is_verifying_golden_patch=True,
         )
         server = SwebenchResourcesServer(config=config, server_client=MagicMock(spec=ServerClient))
         app = server.setup_webserver()
 
         client = TestClient(app)
+
+        monkeypatch.setattr(
+            "resources_servers.swebench.app.AsyncSandbox", lambda *args, **kwargs: MagicMock(start=AsyncMock())
+        )
+        monkeypatch.setattr(
+            "resources_servers.swebench.app.run_instance",
+            AsyncMock(return_value=dict(resolved=True, evaluation_completed=True)),
+        )
+
         res = client.post(
             "/verify",
             json={
-                "repo": "my repo",
+                "repo": "astropy/astropy",
                 "instance_id": "my instance_id",
                 "base_commit": "my base_commit",
                 "patch": "my patch",
@@ -43,7 +56,7 @@ class TestApp:
                 "problem_statement": "my problem_statement",
                 "hints_text": "",
                 "created_at": "my created_at",
-                "version": "my version",
+                "version": "4.3",
                 "FAIL_TO_PASS": "[]",
                 "PASS_TO_PASS": "[]",
                 "environment_setup_commit": "my environment_setup_commit",
