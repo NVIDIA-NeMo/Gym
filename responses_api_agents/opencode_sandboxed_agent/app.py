81a51ae387f4aa4a83cfd2ddca49879e35ecec3a
Brian Yu <bxyu@nvidia.com>
2026-08-04 14:25:36 -0700

    dont fail on conda activate
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- responses_api_agents/opencode_sandboxed_agent/app.py
+++ responses_api_agents/opencode_sandboxed_agent/app.py
@@ -247,7 +247,7 @@ class OpenCodeSandboxedAgent(SimpleResponsesAPIAgent):
         # TODO @bxyu-nvidia: We need to manually activate the conda env here for SWE Verified
         # Eventually this will only be present on the SWE Bench resources server side
         # For now, the activation is put on the harness side.
-        conda_activate_command_str = "source /opt/miniconda3/bin/activate && conda activate testbed"
+        conda_activate_command_str = "(( [[ -f /opt/miniconda3/bin/activate ]] && source /opt/miniconda3/bin/activate && conda activate testbed 2>/dev/null ) || true)"
 
         command = f"""
         echo "Shell: $SHELL" \
