43e6f39591929b078344b0113290553b71845b26
Brian Yu <bxyu@nvidia.com>
2026-08-07 12:44:43 -0700

    try downgrade python script
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

---
+++ benchmarks/nemotron_3.5_super/downgrade_python.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from glob import glob
+from pathlib import Path
+
+
+paths_to_fix = [
+    "pyproject.toml",
+    ".python-version",
+    "uv.lock",
+    *glob("*/*/pyproject.toml", recursive=True),
+]
+print("Downgrading the Python from 3.13.14 to 3.12.13 in the following paths:\n", "\n".join(paths_to_fix))
+for path_to_fix in paths_to_fix:
+    path_to_fix = Path(path_to_fix)
+    content = path_to_fix.read_text()
+    content = content.replace("3.13.14", "3.12.13")
+    path_to_fix.write_text(content)
