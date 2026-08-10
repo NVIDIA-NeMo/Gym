900685286b2421d11facd558b7b040f6923b4889
Brian Yu <bxyu@nvidia.com>
2026-08-10 13:09:26 -0700

    tweaks
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/export_to_csv.py
+++ benchmarks/nemotron_3.5_super/export_to_csv.py
@@ -2,6 +2,8 @@ import json
 from argparse import ArgumentParser
 from csv import DictWriter
 from datetime import datetime
+from subprocess import check_output
+from typing import Union
 from zoneinfo import ZoneInfo
 
 
@@ -19,26 +21,33 @@ with open(aggregate_metrics_fpath) as f:
 agent_to_metrics = {d["agent_ref"]["name"]: d for d in aggregate_metrics}
 
 
-def v(agent: str, value: str) -> float:
-    return agent_to_metrics[agent]["agent_metrics"][value]
+def to_pct(value: float) -> str:
+    return f"{100 * value:.1f}%"
+
+
+def v(agent: str, value: str, is_pct: bool = True) -> Union[float, str]:
+    value = agent_to_metrics[agent]["agent_metrics"][value]
+    return to_pct(value) if is_pct else value
 
 
 row = {
     "Model nickname": "",
-    "Note": "",
     "Model path": args.model_path,
+    "Note": "",
     "Date run": datetime.now(ZoneInfo("America/Los_Angeles")),
-    "Gym commit": None,  # TODO
+    "Gym commit": check_output(["git", "rev-parse", "HEAD"], text=True).strip(),
     "Tau3-Banking": v("tau2_banking_knowledge_bm25_grep_artificial_analysis_agent", "mean/reward"),
-    "Tau3-Average": sum(
-        (
-            v("tau2_benchmark_agent", "airline/reward"),
-            v("tau2_benchmark_agent", "retail/reward"),
-            v("tau2_benchmark_agent", "telecom/reward"),
-            v("tau2_banking_knowledge_bm25_grep_artificial_analysis_agent", "mean/reward"),
+    "Tau3-Average": to_pct(
+        sum(
+            (
+                v("tau2_benchmark_agent", "airline/reward", is_pct=False),
+                v("tau2_benchmark_agent", "retail/reward", is_pct=False),
+                v("tau2_benchmark_agent", "telecom/reward", is_pct=False),
+                v("tau2_banking_knowledge_bm25_grep_artificial_analysis_agent", "mean/reward", is_pct=False),
+            )
         )
-    )
-    / 4,
+        / 4
+    ),
     "SciCode": v("scicode_benchmark_agent", "mean/reward"),
     "AA-LCR": v("aalcr_benchmark_simple_agent", "mean/reward"),
     "AA-Omniscience (OmniIndex)": v("omniscience_omniscience_simple_agent", "mean/reward"),
