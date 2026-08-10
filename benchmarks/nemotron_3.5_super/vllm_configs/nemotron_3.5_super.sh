6be6ebfd9f56ab7127ba281bcb03f224cdc5b2ec
Brian Yu <bxyu@nvidia.com>
2026-08-10 15:52:22 -0700

    bump up max num seqs
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super.sh
+++ benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super.sh
@@ -19,5 +19,5 @@ VLLM_COMMON_ARGS=(
     --model-loader-extra-config '{"enable_multithread_load": true, "num_threads": 96}'
     --enable-expert-parallel
     --max-num-batched-tokens 8480
-    --max-num-seqs 256
+    --max-num-seqs 512
 )
