1db07346cc7c58d5e3e2baf6f8a4f60ecbc784c0
Brian Yu <bxyu@nvidia.com>
2026-08-12 11:09:26 -0700

    try apply to opencode
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- responses_api_agents/opencode_sandboxed_agent/app.py
+++ responses_api_agents/opencode_sandboxed_agent/app.py
@@ -19,7 +19,7 @@ from pathlib import Path
 from shlex import quote
 from time import time
 from traceback import format_exc
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Tuple
 from uuid import uuid4
 
 from fastapi import Request
@@ -48,6 +48,7 @@ from nemo_gym.openai_utils import (
 from nemo_gym.responses_converter import ResponsesConverter
 from nemo_gym.sandbox import AsyncSandbox, SandboxResources, SandboxSpec, create_provider
 from nemo_gym.sandbox.config import resolve_provider_config, resolve_provider_metadata
+from nemo_gym.sandbox.providers.base import SandboxPtySession
 from nemo_gym.server_utils import SESSION_ID_KEY, get_response_json, get_server_url, raise_for_status
 
 
@@ -95,19 +96,22 @@ class OpenCodeSandboxedAgent(SimpleResponsesAPIAgent):
     def model_post_init(self, context: Any, /) -> None:
         super().model_post_init(context)
 
-        self._sandbox_id_to_sandbox: Dict[str, AsyncSandbox] = dict()
+        self._sandbox_id_to_sandbox: Dict[str, Tuple[AsyncSandbox, SandboxPtySession]] = dict()
         self._sandbox_id_to_run_result: Dict[str, Dict[str, Any]] = dict()
 
-    async def _start_sandbox(self, sandbox_id: Optional[str] = None) -> AsyncSandbox:
+    async def _start_sandbox(
+        self, sandbox_id: Optional[str] = None, pty_session_id: Optional[str] = None
+    ) -> Tuple[AsyncSandbox, SandboxPtySession]:
         global_config_dict = get_global_config_dict()
         resolved_sandbox_provider = create_provider(
             resolve_provider_config(self.config.sandbox_provider, global_config_dict)
         )
         provider_default_metadata = resolve_provider_metadata(self.config.sandbox_provider, global_config_dict)
 
-        if sandbox_id:
+        if sandbox_id and pty_session_id:
             sandbox = await AsyncSandbox.connect({"sandbox_id": sandbox_id}, provider=resolved_sandbox_provider)
-            return sandbox
+            pty_session = await sandbox.pty.attach(session_id=pty_session_id, takeover=True)
+            return sandbox, pty_session
 
         if self.config.debug:
             print("Creating new sandbox since one wasn't provided", file=sys.stderr)
@@ -133,7 +137,9 @@ class OpenCodeSandboxedAgent(SimpleResponsesAPIAgent):
         sandbox = AsyncSandbox(resolved_sandbox_provider)
         await sandbox.start(sandbox_spec)
 
-        return sandbox
+        pty_session = await sandbox.pty.create()
+
+        return sandbox, pty_session
 
     def _create_opencode_config(self) -> Dict[str, Any]:
         return {
@@ -247,7 +253,7 @@ class OpenCodeSandboxedAgent(SimpleResponsesAPIAgent):
         request: Request,
         body: NeMoGymResponseCreateParamsNonStreaming = Body(),
     ) -> NeMoGymResponse:
-        sandbox = self._sandbox_id_to_sandbox[request.cookies["sandbox_id"]]
+        sandbox, pty_session = self._sandbox_id_to_sandbox[request.cookies["sandbox_id"]]
 
         query = None
         # This can be modified to handle system/developer prompts too.
@@ -297,8 +303,9 @@ class OpenCodeSandboxedAgent(SimpleResponsesAPIAgent):
             print(f"OpenCode config JSON str: {opencode_config_content}", file=sys.stderr)
 
         try:
-            result = await sandbox.exec(
+            result = await sandbox.pty.exec(
                 command=command,
+                session=pty_session,
                 timeout_s=self.config.sandbox_timeout,
                 env={"OPENCODE_CONFIG_CONTENT": opencode_config_content},
             )
@@ -392,8 +399,11 @@ class OpenCodeSandboxedAgent(SimpleResponsesAPIAgent):
         # @bxyu-nvidia: "sandbox_handle" comes from resources_servers/swebench/app.py
         # Once we graduate to use the sandbox server, this will be in a generic seed_session type that can be model validated.
         seed_session_result = await seed_session_response.json()
-        sandbox = await self._start_sandbox(sandbox_id=seed_session_result.get("sandbox_handle"))
-        self._sandbox_id_to_sandbox[request.session[SESSION_ID_KEY]] = sandbox
+        sandbox, pty_session = await self._start_sandbox(
+            sandbox_id=seed_session_result.get("sandbox_handle"),
+            pty_session_id=seed_session_result.get("pty_session_id"),
+        )
+        self._sandbox_id_to_sandbox[request.session[SESSION_ID_KEY]] = (sandbox, pty_session)
 
         # Propagating the sandbox handle
         cookies["sandbox_id"] = request.session[SESSION_ID_KEY]
