220dc71435926b49106a92012169a76fa9267cc4
Brian Yu <bxyu@nvidia.com>
2026-08-12 08:01:09 -0700

    clean s
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/sbatch_external_vllm.sh
+++ benchmarks/nemotron_3.5_super/sbatch_external_vllm.sh
@@ -42,7 +42,7 @@ cd /opt/Gym
 
 gym eval prepare $@ +use_cached_prepared_benchmarks=true
 
-experiment_name=$EXPERIMENT_NAME/slurm_jobs_id_\$SLURM_JOB_ID/date_\$(date +%Y%m%d_%H%M%S)
+experiment_name=$EXPERIMENT_NAME/slurm_job_id_\$SLURM_JOB_ID/date_\$(date +%Y%m%d_%H%M%S)
 # +uv_venv_dir=/opt/uv_venvs is from the container.
 # +skip_venv_if_present=true will reuse the venvs baked into the container if possible.
 # ++use_absolute_ip=true: Necessary for communication between harness in sandbox and Gym model servers
