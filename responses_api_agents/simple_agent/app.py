26a05f085bb1b2aa6b8a07234865e1c11a7e75ca
Brian Yu <bxyu@nvidia.com>
2026-08-12 17:31:26 -0700

    fix await
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- responses_api_agents/simple_agent/app.py
+++ responses_api_agents/simple_agent/app.py
@@ -318,7 +318,7 @@ class SimpleAgent(SimpleResponsesAPIAgent):
         #     body=body.responses_create_params,
         # )
 
-        response = self.server_client.post(
+        response = await self.server_client.post(
             server_name=self.config.name,
             url_path=self.url_path_for_run("/v1/responses", body),
             json=body.responses_create_params,
