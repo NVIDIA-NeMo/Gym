45534c93894636263f91dcd35a9dfd9583f0ac63
Brian Yu <bxyu@nvidia.com>
2026-08-16 15:43:18 -0700

    start anti cheat setup
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- resources_servers/swebench/app.py
+++ resources_servers/swebench/app.py
@@ -241,6 +241,12 @@ class SwebenchResourcesServer(SimpleResourcesServer):
         # For now, the activation is put on the harness side.
         # await eval_sandbox.exec("source /opt/miniconda3/bin/activate && conda activate testbed")
 
+        # Remove the current Git repo's future history beyond the current commit to prevent the model from cheating.
+        anti_cheat_setup_fpath = Path(__file__).parent / "anti_cheat_setup.sh"
+        await eval_sandbox.upload(anti_cheat_setup_fpath, "/root/.m2/settings.xml")
+        result = await eval_sandbox.exec("""wd=. bash anti_cheat_setup.sh && rm anti_cheat_setup.sh""")
+        assert result.return_code == 0
+
         return SWEBenchSeedSessionResponse(sandbox_handle=eval_sandbox._handle.sandbox_id)
 
     async def verify(self, request: Request, body: SWEBenchVerifyRequest) -> SWEBenchVerifyResponse:
