f5c0f7e37c6b6a5a77e7baece8cb375bb4cc5cdb
Brian Yu <bxyu@nvidia.com>
2026-08-17 21:31:23 -0700

    try apply conetxt window
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- responses_api_agents/opencode_sandboxed_agent/app.py
+++ responses_api_agents/opencode_sandboxed_agent/app.py
@@ -65,6 +65,7 @@ class OpenCodeSandboxedAgentConfig(BaseResponsesAPIAgentConfig):
     remote_opencode_install_script_path: Optional[str] = None
     remote_opencode_binary_path: Optional[str] = None
     opencode_config: Dict[str, Any] = Field(default_factory=dict)
+    max_context_window: int
 
     # Sandbox config
     sandbox_provider: str
@@ -156,12 +157,9 @@ class OpenCodeSandboxedAgent(SimpleResponsesAPIAgent):
                     "models": {
                         "dummy_model": {
                             "limit": {
-                                "context": 0,
-                                "input": 0,
-                                # @bxyu-nvidia: OpenCode defaults to 32k here https://github.com/anomalyco/opencode/blob/58a99916bb96edf5cf605dc03e1be1e4bacf9ff7/packages/opencode/src/provider/transform.ts#L21
-                                # and there is no way to set it to null.
-                                # We set it here to explicitly acknowledge that this parameter is set.
-                                "output": 32_000,
+                                "context": self.config.max_context_window,
+                                "input": self.config.max_context_window,
+                                "output": self.config.max_context_window,
                             },
                         },
                     },
@@ -312,7 +310,10 @@ class OpenCodeSandboxedAgent(SimpleResponsesAPIAgent):
             result = await sandbox.exec(
                 command=command,
                 timeout_s=self.config.sandbox_timeout,
-                env={"OPENCODE_CONFIG_CONTENT": opencode_config_content},
+                env={
+                    "OPENCODE_CONFIG_CONTENT": opencode_config_content,
+                    "OPENCODE_EXPERIMENTAL_OUTPUT_TOKEN_MAX": str(self.config.max_context_window),
+                },
             )
         except:
             result = None
