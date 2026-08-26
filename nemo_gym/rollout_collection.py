7c947de61541ab82ac8d0b4090c2a4f93f495fff
Brian Yu <bxyu@nvidia.com>
2026-08-26 11:28:41 -0700

    try use copy rather than deepcopy
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- nemo_gym/rollout_collection.py
+++ nemo_gym/rollout_collection.py
@@ -21,7 +21,6 @@ import warnings
 from asyncio import Future, Semaphore
 from collections import Counter, defaultdict
 from contextlib import nullcontext
-from copy import deepcopy
 from datetime import timedelta
 from itertools import repeat
 from pathlib import Path
@@ -693,19 +692,23 @@ class RolloutCollectionHelper(BaseModel):
                 continue
 
             for _ in range(row_num_repeats):
-                row = deepcopy(row)
+                repeated_row = row.copy()
 
                 # Resolve rollout index
-                row[ROLLOUT_INDEX_KEY_NAME] = task_idx_to_rollout_idx[row[TASK_INDEX_KEY_NAME]]
+                repeated_row[ROLLOUT_INDEX_KEY_NAME] = task_idx_to_rollout_idx[row[TASK_INDEX_KEY_NAME]]
                 task_idx_to_rollout_idx[row[TASK_INDEX_KEY_NAME]] += 1
 
                 if config.num_repeats_add_seed:
-                    metadata = row[RESPONSES_CREATE_PARAMS_KEY_NAME].setdefault("metadata", {})
+                    repeated_row[RESPONSES_CREATE_PARAMS_KEY_NAME] = repeated_row[
+                        RESPONSES_CREATE_PARAMS_KEY_NAME
+                    ].copy()
+
+                    metadata = repeated_row[RESPONSES_CREATE_PARAMS_KEY_NAME].setdefault("metadata", {})
                     extra_body = json.loads(metadata.get("extra_body", "{}"))
-                    extra_body["seed"] = row[ROLLOUT_INDEX_KEY_NAME]
+                    extra_body["seed"] = repeated_row[ROLLOUT_INDEX_KEY_NAME]
                     metadata["extra_body"] = json.dumps(extra_body)
 
-                rows.append(row)
+                rows.append(repeated_row)
 
         if row_idxs_missing_agent_ref:
             raise ValueError(
