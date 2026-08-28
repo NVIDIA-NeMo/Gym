55d1e66cd5ac7e33ee2d534525f66ca2f2b84a85
Brian Yu <bxyu@nvidia.com>
2026-08-28 10:10:33 -0700

    dont use login shell for main job
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/sbatch_external_vllm.sh
+++ benchmarks/nemotron_3.5_super/sbatch_external_vllm.sh
@@ -274,7 +274,7 @@ main_job_id=$(
         --comment="$SLURM_COMMENT" \
         --exclusive \
         --segment=$NUM_NODES \
-        --wrap 'exec bash -lc "$batch_command"'
+        --wrap 'exec bash -c "$batch_command"'
 )
 main_job_id=${main_job_id%%;*}
 
