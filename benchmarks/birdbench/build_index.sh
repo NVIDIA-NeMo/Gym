90a2bab787a68105617e4bcaa6d6e16543fbb414
Dipendra Misra <dmisra@nvidia.com>
2026-08-29 16:36:51 -0400

    Adding BM25 indexing files
    Signed-off-by: Dipendra Misra <dmisra@nvidia.com>

--- benchmarks/birdbench/build_index.sh
+++ benchmarks/birdbench/build_index.sh
@@ -12,4 +12,4 @@ set -e
 # python process_dataset.py --input_data_file ./data/bird/train/train_enhanced_with_cot.json --output_data_file ./data/train_bird.json --db_path ./data/bird/train/train_databases/ --tables ./data/bird/train/train_tables.json --source bird --mode train --value_limit_num 2 --db_content_index_path ./data/bird/train/db_contents_index
 
 # BIRD (dev)
-python process_dataset.py --input_data_file ./data/bird/dev_20240627/dev.json --output_data_file ./data/dev_bird.json --db_path ./data/bird/dev_20240627/dev_databases/ --tables ./data/bird/dev_20240627/dev_tables.json --source bird --mode dev --value_limit_num 2 --db_content_index_path ./data/bird/dev_20240627/db_contents_index
+python build_index.py --input_data_file ./data/bird/dev_20240627/dev.json --output_data_file ./data/dev_bird.json --db_path ./data/bird/dev_20240627/dev_databases/ --tables ./data/bird/dev_20240627/dev_tables.json --source bird --mode dev --value_limit_num 2 --db_content_index_path ./data/bird/dev_20240627/db_contents_index
