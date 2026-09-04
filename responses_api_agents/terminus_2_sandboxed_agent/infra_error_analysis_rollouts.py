5313fc6b957c9ddbfc58e934190ba7742683efc3
Brian Yu <bxyu@nvidia.com>
2026-09-04 09:46:02 -0700

    cleanup
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- responses_api_agents/terminus_2_sandboxed_agent/infra_error_analysis_rollouts.py
+++ responses_api_agents/terminus_2_sandboxed_agent/infra_error_analysis_rollouts.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 from argparse import ArgumentParser
+from collections import Counter
 
 import orjson
 
@@ -10,11 +11,7 @@ parser.add_argument("--fpath", type=str, required=True)
 args = parser.parse_args()
 
 
-gt_10 = 0
-num_stuck = 0
-num_is_covered = 0
-num_model_call_long = 0
-total = 0
+counts = Counter()
 with open(args.fpath, "rb") as f:
     for i, line in enumerate(f):
         row = orjson.loads(line)
@@ -36,13 +33,20 @@ with open(args.fpath, "rb") as f:
         was_stuck = stuck_count > 10
         model_call_long = row["model_calls_gt_10min"] >= 6
         is_covered = is_gt_10 or was_stuck or model_call_long
+        is_errored = bool(row["error"])
+        is_timeout = is_errored and "raise TimeoutError from exc_val" in row["error"]
         if not is_covered:
             print(f"{i + 1}: {count > 10=} {was_stuck=} {model_call_long=}")
 
-        num_model_call_long += model_call_long
-        num_stuck += was_stuck
-        gt_10 += is_gt_10
-        num_is_covered += is_covered
-        total += 1
+        counts["Long model calls"] += model_call_long
+        counts["Model claims to be stuck"] += was_stuck
+        counts["No valid JSON found in response"] += is_gt_10
+        counts["Samples covered by the above errors"] += is_covered
+        counts["Errored (excluding timeout)"] += is_errored - is_timeout
+        counts["Timed out"] += is_timeout
+        counts["Total samples with reward=0"] += 1
 
-print(gt_10, num_stuck, num_model_call_long, num_is_covered, total)
+print_str = ""
+for k, v in counts.items():
+    print_str += f"{k}: {v}\n"
+print(print_str)
