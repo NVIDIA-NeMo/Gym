abe27842fa03fb9e7379601c27387f10cff71695
Christian Munley <cmunley@nvidia.com>
2026-09-09 04:15:41 -0700

    update
    Signed-off-by: Christian Munley <cmunley@nvidia.com>
    (cherry picked from commit d2798e6f8e10e180c412e10db3c704ddf6bba654)
    Signed-off-by: Daniele Molinari <dmolinari@nvidia.com>

--- environments/ab_aa/prepare.py
+++ environments/ab_aa/prepare.py
@@ -11,11 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-    python environments/ab_aa/prepare.py
-    python environments/ab_aa/prepare.py --size 10 --domains sales
-"""
-
 from __future__ import annotations
 
 import argparse
