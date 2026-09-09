a6c2a443a4d2999faceed5b1af905cfd4a035f59
Frankie Siino <fsiino@nvidia.com>
2026-09-09 21:07:06 +0000

    Restore comment from main
    Signed-off-by: Frankie Siino <fsiino@nvidia.com>

--- benchmarks/nemotron_3.5_super/sbatch_external_vllm.sh
+++ benchmarks/nemotron_3.5_super/sbatch_external_vllm.sh
@@ -288,6 +288,9 @@ else
     # Preserve main's independent topology for models that fit one complete
     # tensor-parallel replica on each node.
     if (( SLURM_PROCID == 0 )); then
+        # @bxyu-nvidia: for --intra-node-data-parallel-size: Not sure what to set this to other than 1. I can't tell from the docs what is appropriate and 1 seems to work fine.
+        # Set a super long request timeout since some reasoning requests may take a long time to generate.
+        # Don't manually wait as vllm-router will wait for the URLs to come up
         router_args=( \
             --prefill-policy cache_aware \
             --decode-policy cache_aware \
