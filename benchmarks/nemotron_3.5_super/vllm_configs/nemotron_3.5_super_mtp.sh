879b267b5e05c2340d3cccf554e5a2b61d9f1f88
Brian Yu <bxyu@nvidia.com>
2026-09-10 10:58:14 -0700

    reduce max num batched tokens
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super_mtp.sh
+++ benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super_mtp.sh
@@ -32,7 +32,7 @@ VLLM_COMMON_ARGS=(
 )
 VLLM_PREFILL_ARGS=(
     --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_producer","kv_load_failure_policy":"fail"}'
-    --max-num-batched-tokens 135680
+    --max-num-batched-tokens 67840
     --max-num-seqs 1024
     --data-parallel-size-local 1
     --tensor-parallel-size 4
