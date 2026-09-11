3a9c8df266a78e02e0fb7a7a8fe1dc4d6ec7d6c2
Brian Yu <bxyu@nvidia.com>
2026-09-11 15:33:48 -0700

    fix dp size
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/sbatch_external_vllm_data_parallel.sh
+++ benchmarks/nemotron_3.5_super/sbatch_external_vllm_data_parallel.sh
@@ -169,7 +169,7 @@ if (( SLURM_PROCID == 0 )); then
         "\${VLLM_COMMON_ARGS[@]}" "\${VLLM_PREFILL_ARGS[@]}" \
         --host \$this_node_hostname \
         --port $PREFILL_SERVER_PORT \
-        --data-parallel-size $NUM_PREFILL_NODES \
+        --data-parallel-size $((NUM_PREFILL_NODES * 4)) \
         --data-parallel-address \$PREFILL_HEAD \
         --data-parallel-rpc-port $PREFILL_DP_RPC_PORT \
         --api-server-count 1
@@ -180,7 +180,7 @@ elif (( SLURM_PROCID < $NUM_PREFILL_NODES )); then
     vllm serve "$MODEL" --served-model-name "$MODEL_NAME" \
         "\${VLLM_COMMON_ARGS[@]}" "\${VLLM_PREFILL_ARGS[@]}" \
         --headless \
-        --data-parallel-size $NUM_PREFILL_NODES \
+        --data-parallel-size $((NUM_PREFILL_NODES * 4)) \
         --data-parallel-start-rank \$SLURM_PROCID \
         --data-parallel-address \$PREFILL_HEAD \
         --data-parallel-rpc-port $PREFILL_DP_RPC_PORT
@@ -193,7 +193,7 @@ elif (( SLURM_PROCID == $NUM_PREFILL_NODES )); then
         "\${VLLM_COMMON_ARGS[@]}" "\${VLLM_DECODE_ARGS[@]}" \
         --host \$this_node_hostname \
         --port $DECODE_SERVER_PORT \
-        --data-parallel-size $NUM_DECODE_NODES \
+        --data-parallel-size $((NUM_DECODE_NODES * 4)) \
         --data-parallel-address \$DECODE_HEAD \
         --data-parallel-rpc-port $DECODE_DP_RPC_PORT \
         --api-server-count 1
@@ -205,7 +205,7 @@ else
     vllm serve "$MODEL" --served-model-name "$MODEL_NAME" \
         "\${VLLM_COMMON_ARGS[@]}" "\${VLLM_DECODE_ARGS[@]}" \
         --headless \
-        --data-parallel-size $NUM_DECODE_NODES \
+        --data-parallel-size $((NUM_DECODE_NODES * 4)) \
         --data-parallel-start-rank \$(( SLURM_PROCID - $NUM_PREFILL_NODES )) \
         --data-parallel-address \$DECODE_HEAD \
         --data-parallel-rpc-port $DECODE_DP_RPC_PORT
