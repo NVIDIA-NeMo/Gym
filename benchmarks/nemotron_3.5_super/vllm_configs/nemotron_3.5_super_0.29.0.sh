b072cc7408b05338fa52e31ed5961947c3de253d
Brian Yu <bxyu@nvidia.com>
2026-09-11 16:03:51 -0700

    try mamba cache mode all
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super_0.29.0.sh
+++ benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super_0.29.0.sh
@@ -21,7 +21,7 @@ VLLM_COMMON_ARGS=(
     --kv-cache-dtype fp8
     --no-disable-hybrid-kv-cache-manager
     --block-size 128
-    --mamba-cache-mode align
+    --mamba-cache-mode all
     --mamba-ssm-cache-dtype float32
     --model-loader-extra-config '{"enable_multithread_load": true, "num_threads": 96}'
     --enable-expert-parallel
