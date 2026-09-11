31dc80bc318b38f7d650559fad3cb6ac358d8579
Christian Munley <cmunley@nvidia.com>
2026-09-10 19:05:17 -0700

    update
    Signed-off-by: Christian Munley <cmunley@nvidia.com>

--- nemo_gym/openai_utils.py
+++ nemo_gym/openai_utils.py
@@ -976,7 +976,7 @@ class NeMoGymChatCompletionAssistantMessageParam(ChatCompletionAssistantMessageP
     # Override the iterable which is annoying to work with.
     content: Union[str, List[ContentArrayOfContentPart], None]
     tool_calls: Optional[NeMoGymChatCompletionMessageToolCallsParam] = None
-    # Accept Verifiers' null compatibility field without forwarding it to model servers.
+    # Allow incoming responses with reasoning_content=None; this field should not be used.
     reasoning_content: Annotated[None, Field(exclude=True)]
 
 
