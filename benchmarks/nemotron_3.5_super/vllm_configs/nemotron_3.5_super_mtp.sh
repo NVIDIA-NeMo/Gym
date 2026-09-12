bf1357374662d7d30c9c629084998cd39ddcf8c9
Brian Yu <bxyu@nvidia.com>
2026-09-11 18:47:29 -0700

    try no fine graind pc
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super_mtp.sh
+++ benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super_mtp.sh
@@ -6,6 +6,8 @@ GYM_MODEL_PARAMS=(
 )
 
 # @bxyu-nvidia: `--skip-mm-profiling` Is needed to get Super VL checkpoint working, even with text benchmarks
+#     --enable-mamba-fine-grained-prefix-cache
+#     --prefix-match-unit 16
 VLLM_COMMON_ARGS=(
     --trust-remote-code
     --disable-uvicorn-access-log
@@ -29,8 +31,6 @@ VLLM_COMMON_ARGS=(
     --data-parallel-size 1
     --api-server-count 1
     --speculative-config '{"method":"mtp","num_speculative_tokens":5}'
-    --enable-mamba-fine-grained-prefix-cache
-    --prefix-match-unit 16
     --prefix-cache-retention-interval None
 )
 VLLM_PREFILL_ARGS=(
