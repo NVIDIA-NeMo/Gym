1a293d2b0fbdcf9ce4aa2e8517b86e06a983e5dd
Brian Yu <bxyu@nvidia.com>
2026-09-13 11:48:49 -0700

    fix reservation and qos
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/custom_vllm_version.sh
+++ benchmarks/nemotron_3.5_super/custom_vllm_version.sh
@@ -132,6 +132,8 @@ if ! srun \
     --time="${SLURM_TIME:-01:00:00}" \
     --container-image="${BASE_IMAGE}" \
     --container-mounts="${MOUNTS}" \
+    --reservation=sla_res_n3_post \
+    --qos=hero-res \
     --container-save="${OUT_SQSH}" \
     --export=ALL \
     bash "${SNAP}" __inside_build
