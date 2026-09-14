2fcead7ea41efdbd2950c27a27396b94a3daf842
Brian Yu <bxyu@nvidia.com>
2026-09-14 13:41:25 -0700

    try save more memory
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super_mtp.sh
+++ benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super_mtp.sh
@@ -9,7 +9,7 @@ GYM_MODEL_PARAMS=(
 VLLM_COMMON_ARGS=(
     --trust-remote-code
     --disable-uvicorn-access-log
-    --gpu-memory-utilization 0.85
+    --gpu-memory-utilization 0.9
     --distributed-executor-backend mp
     --data-parallel-backend mp
     --enable-auto-tool-choice
@@ -33,9 +33,10 @@ VLLM_COMMON_ARGS=(
 )
 VLLM_PREFILL_ARGS=(
     --speculative-config '{"method":"mtp","num_speculative_tokens":5,"num_speculative_tokens_per_batch_size":[[1,1024,0]]}'
+    --compilation-config '{"cudagraph_mode":"PIECEWISE"}'
     --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_producer","kv_load_failure_policy":"fail","kv_connector_extra_config":{"kv_lease_duration":180}}'
-    --max-num-batched-tokens 33920
-    --max-num-seqs 1024
+    --max-num-batched-tokens 16960
+    --max-num-seqs 256
     --data-parallel-size-local 1
     --tensor-parallel-size 4
 )
@@ -43,8 +44,8 @@ VLLM_DECODE_ARGS=(
     --speculative-config '{"method":"mtp","num_speculative_tokens":5}'
     --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_consumer","kv_load_failure_policy":"fail","kv_connector_extra_config":{"kv_lease_duration":180}}'
     --compilation-config '{"cudagraph_mode":"FULL_DECODE_ONLY"}'
-    --max-num-batched-tokens 33920
-    --max-num-seqs 1024
+    --max-num-batched-tokens 16960
+    --max-num-seqs 256
     --data-parallel-size-local 1
     --tensor-parallel-size 4
 )
