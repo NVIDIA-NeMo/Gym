b06592ea121d55396aada28d9ae1114d34468386
Brian Yu <bxyu@nvidia.com>
2026-09-14 10:36:30 -0700

    revert to baseline config; add cuda graph capture size
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super_mtp.sh
+++ benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super_mtp.sh
@@ -31,16 +31,17 @@ VLLM_COMMON_ARGS=(
     --speculative-config '{"method":"mtp","num_speculative_tokens":5}'
     --enable-mamba-fine-grained-prefix-cache
     --prefix-match-unit 16
+    --max-cudagraph-capture-size 1200
 )
 VLLM_PREFILL_ARGS=(
-    --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_producer","kv_load_failure_policy":"fail","kv_connector_extra_config":{"kv_lease_duration":180}}'
-    --max-num-batched-tokens 33920
+    --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_producer","kv_load_failure_policy":"fail"}'
+    --max-num-batched-tokens 135680
     --max-num-seqs 1024
     --data-parallel-size-local 1
     --tensor-parallel-size 4
 )
 VLLM_DECODE_ARGS=(
-    --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_consumer","kv_load_failure_policy":"fail","kv_connector_extra_config":{"kv_lease_duration":180}}'
+    --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_consumer","kv_load_failure_policy":"fail"}'
     --compilation-config '{"cudagraph_mode":"FULL_DECODE_ONLY"}'
     --max-num-batched-tokens 33920
     --max-num-seqs 1024
