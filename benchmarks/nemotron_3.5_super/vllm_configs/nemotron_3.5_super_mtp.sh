a67735b0f8f61575bcc45146576784948dafec34
Brian Yu <bxyu@nvidia.com>
2026-09-15 13:11:02 -0700

    api server count 1
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super_mtp.sh
+++ benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_super_mtp.sh
@@ -29,7 +29,7 @@ VLLM_COMMON_ARGS=(
     --data-parallel-size 1
     --data-parallel-size-local 1
     --tensor-parallel-size 4
-    --api-server-count 16
+    --api-server-count 1
     --enable-mamba-fine-grained-prefix-cache
     --prefix-match-unit 16
 )
