6156a4fb477645cf06114752a08ebc9f75a67f8e
Brian Yu <bxyu@nvidia.com>
2026-09-15 18:24:21 -0700

    tweak dsv4 flash config
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/vllm_configs/deepseek_v4_flash.sh
+++ benchmarks/nemotron_3.5_super/vllm_configs/deepseek_v4_flash.sh
@@ -9,35 +9,31 @@ VLLM_COMMON_ARGS=(
     --distributed-executor-backend mp
     --data-parallel-backend mp
     --enable-auto-tool-choice
-    --tool-call-parser qwen3_coder
-    --reasoning-parser nemotron_v3
+    --tool-call-parser deepseek_v4
+    --reasoning-parser deepseek_v4
+    --reasoning-config '{"reasoning_parser":"deepseek_v4","reasoning_start_str":"","reasoning_end_str":""}'
+    --tokenizer-mode deepseek_v4
     --enable-chunked-prefill
     --enable-prefix-caching
-    --max-model-len 262144
     --kv-cache-dtype fp8
     --no-disable-hybrid-kv-cache-manager
-    --block-size 128
-    --mamba-cache-mode align
-    --mamba-ssm-cache-dtype float32
+    --block-size 256
     --model-loader-extra-config '{"enable_multithread_load": true, "num_threads": 96}'
     --enable-expert-parallel
-    --skip-mm-profiling
     --data-parallel-size 1
     --data-parallel-size-local 1
     --tensor-parallel-size 4
     --api-server-count 1
-    --enable-mamba-fine-grained-prefix-cache
-    --prefix-match-unit 16
+    --attention_config.use_fp4_indexer_cache True
+    --moe-backend deep_gemm_mega_moe
+    --speculative-config '{"method":"dspark","num_speculative_tokens":7,"draft_sample_method":"probabilistic"}'
 )
 VLLM_PREFILL_ARGS=(
-    --speculative-config '{"method":"mtp","num_speculative_tokens":5,"num_speculative_tokens_per_batch_size":[[1,1024,0]]}'
-    --max-cudagraph-capture-size 1200
     --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_producer","kv_load_failure_policy":"fail","kv_connector_extra_config":{"kv_lease_duration":180}}'
     --max-num-batched-tokens 33920
     --max-num-seqs 1024
 )
 VLLM_DECODE_ARGS=(
-    --speculative-config '{"method":"mtp","num_speculative_tokens":5}'
     --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_consumer","kv_load_failure_policy":"fail","kv_connector_extra_config":{"kv_lease_duration":180}}'
     --compilation-config '{"cudagraph_mode":"FULL_DECODE_ONLY"}'
     --max-num-batched-tokens 33920
