4ec93c729c1ffacd6f0c4e55f70de1906df78ea6
Brian Yu <bxyu@nvidia.com>
2026-09-18 22:26:26 -0700

    raise mnbt
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3_ultra.sh
+++ benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3_ultra.sh
@@ -45,7 +45,7 @@ VLLM_PREFILL_ARGS=(
     --speculative-config '{"method":"mtp","num_speculative_tokens":5,"num_speculative_tokens_per_batch_size":[[1,1024,0]]}'
     --max-cudagraph-capture-size 1200
     --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_producer","kv_load_failure_policy":"fail","kv_connector_extra_config":{"kv_lease_duration":180}}'
-    --max-num-batched-tokens 33920
+    --max-num-batched-tokens 135680
     --max-num-seqs 1024
 )
 VLLM_DECODE_ARGS=(
