a86042896c5eb27e6d8c4a5a8182549599b99bed
Brian Yu <bxyu@nvidia.com>
2026-09-21 13:41:19 -0700

    update lightning config
    Signed-off-by: Brian Yu <bxyu@nvidia.com>

--- benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_lightning.sh
+++ benchmarks/nemotron_3.5_super/vllm_configs/nemotron_3.5_lightning.sh
@@ -15,7 +15,7 @@ export VLLM_USE_V2_MODEL_RUNNER=0
 VLLM_COMMON_ARGS=(
     --trust-remote-code
     --disable-uvicorn-access-log
-    --gpu-memory-utilization 0.9
+    --gpu-memory-utilization 0.85
     --distributed-executor-backend mp
     --data-parallel-backend mp
     --enable-auto-tool-choice
@@ -25,27 +25,24 @@ VLLM_COMMON_ARGS=(
     --enable-prefix-caching
     --kv-cache-dtype fp8
     --no-disable-hybrid-kv-cache-manager
-    --no-async-scheduling
     --block-size 128
     --mamba-cache-mode align
     --mamba-ssm-cache-dtype float32
     --model-loader-extra-config '{"enable_multithread_load": true, "num_threads": 96}'
     --enable-expert-parallel
     --data-parallel-size 1
+    --data-parallel-size-local 1
+    --tensor-parallel-size 4
     --api-server-count 1
 )
 VLLM_PREFILL_ARGS=(
-    --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_producer","kv_load_failure_policy":"fail"}'
-    --max-num-batched-tokens 135680
+    --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_producer","kv_load_failure_policy":"fail","kv_connector_extra_config":{"kv_lease_duration":180}}'
+    --max-num-batched-tokens 33920
     --max-num-seqs 1024
-    --data-parallel-size-local 1
-    --tensor-parallel-size 4
 )
 VLLM_DECODE_ARGS=(
-    --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_consumer","kv_load_failure_policy":"fail"}'
+    --kv-transfer-config '{"kv_connector":"NixlConnector","kv_role":"kv_consumer","kv_load_failure_policy":"fail","kv_connector_extra_config":{"kv_lease_duration":180}}'
     --compilation-config '{"cudagraph_mode":"FULL_DECODE_ONLY"}'
     --max-num-batched-tokens 33920
     --max-num-seqs 1024
-    --data-parallel-size-local 1
-    --tensor-parallel-size 4
 )
