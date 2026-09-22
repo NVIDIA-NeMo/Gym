a8fdec270ba5e3f43a7ed6872104584ca9ebe8a7
Christian Munley <cmunley@nvidia.com>
2026-09-22 09:43:11 -0700

    update
    Signed-off-by: Christian Munley <cmunley@nvidia.com>

--- nemo_gym/openai_utils.py
+++ nemo_gym/openai_utils.py
@@ -1138,8 +1138,6 @@ NeMoGymChatCompletionMessageToolCallsParam: TypeAlias = Annotated[
 class NeMoGymChatCompletionAssistantMessageParam(ChatCompletionAssistantMessageParam, total=False):
     # Override the iterable which is annoying to work with.
     content: Union[str, List[ContentArrayOfContentPart], None]
-    reasoning: NotRequired[str]
-    reasoning_content: NotRequired[str]
     tool_calls: Optional[NeMoGymChatCompletionMessageToolCallsParam] = None
     # Allow incoming responses with reasoning_content=None. This field should not be used.
     reasoning_content: Annotated[None, Field(exclude=True)]
